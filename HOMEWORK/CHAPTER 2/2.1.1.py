052b105adff6ca6a7e464360f0836979bac34c26
Nikitosina3000 <krevedga@mail.ru>
2021-10-11 00:05:38 +0300

    ad

--- HOMEWORK/CHAPTER 2/2.1.1.py
+++
@@ -1,4 +0,0 @@
-def greetings(name):
-    a = ('Доброго времени суток, ' + name.split()[0] + " \"Человек\" " + name.split()[1])
-    return a
-print(greetings(input()))
--- HOMEWORK/CHAPTER 2/2.1.3.py
+++
@@ -1,4 +0,0 @@
-def debug_control(*args):
-    print(len(args))
-
-debug_control(1,"WELCOME","TO","THE","CLUB, ","BUDDY",1)
